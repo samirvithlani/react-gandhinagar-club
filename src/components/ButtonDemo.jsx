362645686bcbed290f8ce7481b478d1964b2c17f
samirvithlani <samir.vithlani83955@gmail.com>
2024-05-25 13:53:52 +0530

    button

---
+++ src/components/ButtonDemo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const ButtonDemo = () => {
+  return (
+    <div>
+        {/* <button>Click me</button> */}
+        {/* <MyButton /> */} //clickme
+        {/* <MyButton /> */} //delete
+    </div>
+  )
+}
